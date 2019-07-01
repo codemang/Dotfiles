ca6ff62ea232767b617d4613522f85d53a9d26a6
Nathaniel Rubin <nateboop@gmail.com>
2019-07-01 09:46:07 -0400

    Add shortcuts for docker

---
+++ zsh/docker.zsh
@@ -0,0 +1,7 @@
+alias dc="docker-compose"
+alias dcd="docker-compose down"
+alias dcu="docker-compose up"
+alias dcl="docker-compose logs -f"
+alias dcr="docker-compose run"
+
+alias dm="docker-machine"
