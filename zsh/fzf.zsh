eb4396ed5e16a5d057b850c761d3e45574925d84
Nathaniel Rubin <nateboop@gmail.com>
2019-11-04 10:23:48 -0500

    use ripgrep for fzf

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,5 +1,4 @@
 source "$HOME/.fzf.zsh"
-export FZF_COMPLETION_TRIGGER=',,'
-export FZF_DEFAULT_COMMAND='ag -g ""'
+export FZF_DEFAULT_COMMAND='rg --files'
 export FZF_DEFAULT_OPTS='--height 80%'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
