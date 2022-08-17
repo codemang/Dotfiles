9d130912773f98b484fd66c30a58d5e67ebd0b78
Nathaniel Rubin <nateboop@gmail.com>
2022-08-17 14:41:55 -0400

    Hardcode my Ruby installation to v2.7.2

--- bootstrap/ruby.rb
+++ bootstrap/ruby.rb
@@ -1,14 +1,8 @@
 class Ruby
+  DEFAULT_RUBY_VERSION = '2.7.2'
   # Depends on chruby and ruby-install being installed
   def self.install
-    system('ruby-install ruby')
-
-    latest_ruby = `ls ~/.rubies`
-      .split("\n")
-      .map(&:strip)
-      .sort_by{ |ruby_version| ruby_version }
-      .last
-
-    `echo #{latest_ruby} > ~/.ruby-version`
+    system("ruby-install ruby #{DEFAULT_RUBY_VERSION}")
+    `echo #{DEFAULT_RUBY_VERSION} > ~/.ruby-version`
   end
 end
