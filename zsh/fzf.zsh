28229144d3f5b65e8516113d40e6b0dff0cc2103
Nathaniel Rubin <nateboop@gmail.com>
2022-08-17 14:53:03 -0400

    Update default FZF command to follow symlinks

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,4 +1,5 @@
 source "$HOME/.fzf.zsh"
-export FZF_DEFAULT_COMMAND='rg --files'
+# -L follows symlinks
+export FZF_DEFAULT_COMMAND='rg --files -L'
 export FZF_DEFAULT_OPTS='--height 80%'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
