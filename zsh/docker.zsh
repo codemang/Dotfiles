6e944471c5c4b027c361a6659f05a8f9018ef109
Nathaniel Rubin <nateboop@gmail.com>
2022-08-17 14:53:28 -0400

    Add a few docker utils

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -3,6 +3,15 @@ alias dcb="docker-compose build"
 alias dcd="docker-compose down"
 alias dcu="docker-compose up"
 alias dcl="docker-compose logs -f"
-alias dcr="docker-compose run --rm"
+alias dcr="docker-compose run --rm --use-aliases"
+alias dcrp="docker-compose run --rm --service-ports"
 
 alias dm="docker-machine"
+
+dk() {
+  docker kill $(docker ps -q)
+}
+
+dr() {
+  docker rm $(docker ps -a -q)
+}
