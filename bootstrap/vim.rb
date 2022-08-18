e1927455006534f35af87ba4e0d9474900b22965
Nathaniel Rubin <nateboop@gmail.com>
2022-08-18 11:34:50 -0400

    Enable VimPlug to auto-download itself.

--- bootstrap/vim.rb
+++ bootstrap/vim.rb
@@ -3,15 +3,7 @@ class Vim
   # Support macvim and neovim
 
   def self.install
-    install_plugin_manager
     install_vim_plugins
-  end
-
-  def self.install_plugin_manager
-    plug_dir = File.join(Dir.home, '.local/share/nvim/site/autoload/plug.vim')
-    if !File.file?(plug_dir)
-      system "curl -fLo #{plug_dir} --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim"
-    end
   end
 
   def self.install_vim_plugins
