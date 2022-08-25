e29797dd8e55b4e15bf5944a147207fd3f722f64
Nathaniel Rubin <nateboop@gmail.com>
2022-08-25 14:02:02 -0400

    Add some more basic Vim config to the new Lua config

--- dotfiles/nvim/after/plugin/defaults.lua
+++ dotfiles/nvim/after/plugin/defaults.lua
@@ -14,7 +14,6 @@ api.nvim_set_keymap('i', 'kj', '<Esc>', {})
 opt.termguicolors = true -- Enable colors in terminal
 opt.hlsearch = true --Set highlight on search
 opt.number = true --Make line numbers default
-opt.relativenumber = true --Make relative number default
 opt.mouse = "a" --Enable mouse mode
 opt.breakindent = true --Enable break indent
 opt.undofile = true --Save undo history
@@ -24,9 +23,23 @@ opt.updatetime = 250 --Decrease update time
 opt.signcolumn = "yes" -- Always show sign column
 opt.clipboard = "unnamedplus" -- Access system clipboard
 
+opt.expandtab = true
 opt.tabstop = 2
-opt.softtabstop = 2
 opt.shiftwidth = 2
+opt.softtabstop = 2
+opt.autoindent = true
+opt.smartindent = true
+opt.shiftround = true
+opt.smarttab = true
+
+-- No swp files
+vim.api.nvim_command('set noswapfile')
+vim.api.nvim_command('set nobackup')
+vim.api.nvim_command('set nowb')
+
+-- When using "y" to copy from Vim, copy to the global Clipboard used by
+-- all apps. https://github.com/tmux/tmux/issues/543
+opt.clipboard = 'unnamed'
 
 api.nvim_set_keymap('n', '<Leader>w', ':w<Cr>', {})
 api.nvim_set_keymap('n', '<Leader>wq', ':wq<Cr>', {})
