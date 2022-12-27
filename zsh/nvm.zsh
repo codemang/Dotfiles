bd36287dd92af0433b831742564220802ebd278c
Nathaniel Rubin <nateboop@gmail.com>
2022-12-27 00:58:07 -0500

    Speed up boot of zshrc

--- zsh/nvm.zsh
+++ zsh/nvm.zsh
@@ -1,28 +1,22 @@
 # https://github.com/nvm-sh/nvm
 # Basic NVM setup
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 
-# NVM will automatically update the current node version if there is a .nvmrc file
-# https://github.com/nvm-sh/nvm#calling-nvm-use-automatically-in-a-directory-with-a-nvmrc-file
-autoload -U add-zsh-hook
-load-nvmrc() {
-  local node_version="$(nvm version)"
-  local nvmrc_path="$(nvm_find_nvmrc)"
-
-  if [ -n "$nvmrc_path" ]; then
-    local nvmrc_node_version=$(nvm version "$(cat "${nvmrc_path}")")
-
-    if [ "$nvmrc_node_version" = "N/A" ]; then
-      nvm install
-    elif [ "$nvmrc_node_version" != "$node_version" ]; then
-      nvm use
-    fi
-  elif [ "$node_version" != "$(nvm version default)" ]; then
-    echo "Reverting to nvm default version"
-    nvm use default
-  fi
+# https://github.com/nvm-sh/nvm/issues/2724#issuecomment-1336537635
+laxy-load-nvmrc() {
+  # Lazy load
+  # https://github.com/nvm-sh/nvm/issues/2724
+  unset -f node nvm
+  export NVM_DIR="$HOME/.nvm"
+  [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"  # This loads nvm
+  [ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"  # This loads nvm bash_completion
 }
-add-zsh-hook chpwd load-nvmrc
-load-nvmrc
+
+node() {
+  lazy_load_nvm
+  node $@
+}
+
+nvm() {
+  lazy_load_nvm
+  node $@
+}
