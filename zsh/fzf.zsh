6e75db92ba84d29edee6ca8aaf9acc6375acd3f9
Nathaniel Rubin <nateboop@gmail.com>
2023-01-06 23:08:19 -0500

    Fix issue where fzf was loaded before required env vars were set

--- zsh/fzf.zsh
+++ zsh/fzf.zsh
@@ -1,5 +1,6 @@
-source "$HOME/.fzf.zsh"
 # -L follows symlinks
 export FZF_DEFAULT_COMMAND='rg --files -L'
 export FZF_DEFAULT_OPTS='--height 80%'
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
+
+source "$HOME/.fzf.zsh"
