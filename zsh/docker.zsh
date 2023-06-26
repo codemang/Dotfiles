0b2a47ffe8749544c1a747791bb501cac204433c
Nate Rubin <nrubin19@bloomberg.net>
2023-06-26 15:21:40 -0400

    Sync docker helper utils from work

--- zsh/docker.zsh
+++ zsh/docker.zsh
@@ -4,13 +4,22 @@ alias dcd="docker-compose down"
 alias dcu="docker-compose up"
 alias dcl="docker-compose logs -f"
 alias dcr="docker-compose run --rm --use-aliases"
+alias dce="docker-compose exec"
 alias dcrp="docker-compose run --rm --service-ports"
 alias dcps="docker-compose ps"
+alias dm="docker-machine"
 
-dk() {
+function dk() {
   docker kill $(docker ps -q)
 }
 
-dr() {
+function dr() {
   docker rm $(docker ps -a -q)
+}
+
+function derrors() {
+  # https://stackoverflow.com/a/55990412
+  container_name=$(docker inspect -f '{{.Name}}' $(docker-compose ps -q $1) | cut -c2-)
+
+  docker inspect --format "{{json .State.Health }}" $container_name | jq
 }
