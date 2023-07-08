c4b72aefef5d2fe3b82112f11935d3dc1ec2f26b
Nate Rubin <nrubin19@bloomberg.net>
2023-07-07 22:15:06 -0400

    Rename the restart tmux bash alias

--- zsh/tmux.zsh
+++ zsh/tmux.zsh
@@ -7,14 +7,4 @@ alias ts="tmux switch -t"
 alias td="tmux detach"
 alias tk="tmux kill-session -t"
 alias tname="tmux display-message -p '#S'"
-alias restart_tmux="tmux kill-server"
-
-function lay() {
-  tmux split-window -h
-  tmux split-window -v
-  tmux split-window -h
-  tmux select-pane -t 2
-  tmux split-window -h
-  tmux select-pane -t 1
-  tmux split-window -h
-}
+alias rtmux="tmux kill-server"
