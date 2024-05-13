8fcee780d1466df1bb50df9b866171eb39f75bc3
Nate Rubin <nrubin19@bloomberg.net>
2024-05-13 09:39:16 -0400

    Add vim startup command to jump to last edit position

--- nvim_chad/init.lua
+++ nvim_chad/init.lua
@@ -46,6 +46,14 @@ vim.cmd [[
   augroup end
 ]]
 
+-- Jump to last edit position on opening file
+vim.api.nvim_create_autocmd('BufReadPost', {
+  desc = 'Open file at the last position it was edited earlier',
+  group = misc_augroup,
+  pattern = '*',
+  command = 'silent! normal! g`"zv'
+})
+
 -- vim.cmd [[
 --   augroup custom_highlight
 --     autocmd!
