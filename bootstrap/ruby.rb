5d493f1f92b11dfa5e1e5715daba59aae7d41668
Nathaniel Rubin <nateboop@gmail.com>
2024-12-12 20:17:01 -0500

    Revert "Update ruby install steps"
    This reverts commit b646b67c0e6de50b7075a645043dbd9a11bb821b.

--- bootstrap/ruby.rb
+++ bootstrap/ruby.rb
@@ -1,7 +1,9 @@
 class Ruby
+  DEFAULT_RUBY_VERSION = '2.7.2'.freeze
+
   # Depends on chruby and ruby-install being installed
   def self.install
-    system("ruby-install ruby")
-    `echo $(ls ~/.rubies | head) > ~/.ruby-version`
+    system("ruby-install ruby #{DEFAULT_RUBY_VERSION}")
+    `echo #{DEFAULT_RUBY_VERSION} > ~/.ruby-version`
   end
 end
